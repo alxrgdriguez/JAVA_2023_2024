aa2de1b9b9200347cac5ccd7e346959a56726474
alxrgdriguez <sralexred@gmail.com>
2023-11-06 12:02:15 +0100

    Ejercicio8_Repaso

--- EjerciciosRepaso/Ejercicio8.java
+++ EjerciciosRepaso/Ejercicio8.java
@@ -8,19 +8,24 @@ public class Ejercicio8 {
      */
     public static void main(String[] args) {
 
+            //Declaramos la variable de valor entero
             int terminos = 0;
 
             // Imprime los primeros 20 términos de la serie
             for (int i = 1; i <= 20; i++) {
             // Calcula el término actual de la serie
            
+            //En el ejercicio se puede ver que los numeros negativos son pares, así que hacemos la condción para todos esos numeros pares
+            //así los muestre con un (-).
             if (i % 2 == 0) {
                 terminos = -i;
+            
+            //Sino se muestran normales o (positivos)
             } else {
                 terminos = i;
             }
 
-            // Imprime el término
+            // Imprimimos los terminos se parados con un espacio
             System.out.print(terminos + " ");
             }
     }
