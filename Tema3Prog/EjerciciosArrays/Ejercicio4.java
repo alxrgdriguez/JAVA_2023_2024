331cbd86ccb56688820c0492f4e41d6ef7f19e94
alxrgdriguez <sralexred@gmail.com>
2023-11-07 21:04:45 +0100

    Ejercicios_Arrays

--- Tema3Prog/EjerciciosArrays/Ejercicio4.java
+++ Tema3Prog/EjerciciosArrays/Ejercicio4.java
@@ -1,8 +1,23 @@
 package Tema3Prog.EjerciciosArrays;
 
+import java.util.Scanner;
+
 public class Ejercicio4 {
 
     public static void main(String[] args) {
+
+        //Crear un array llamado meses y rellenarlo con el numero de días que tiene cada mes
+        
+        /*int meses[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+        int mes = 0;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Dime un numero de 1 al 12: ");
+        mes = Integer.parseInt(sc.nextLine());*/
+
+  
+
         
     }
     
