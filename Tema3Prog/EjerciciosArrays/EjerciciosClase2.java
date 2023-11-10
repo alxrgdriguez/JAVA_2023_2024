0ddc56ab87f98fc212a4f72b49777ed4ca14c538
alxrgdriguez <sralexred@gmail.com>
2023-11-10 17:19:36 +0100

    EjerciciosClase1

---
+++ Tema3Prog/EjerciciosArrays/EjerciciosClase2.java
@@ -0,0 +1,15 @@
+package Tema3Prog.EjerciciosArrays;
+
+public class EjerciciosClase2 {
+
+    public static void main(String[] args) {
+        
+        /*2. Lanza 5 dados de póker (. J Q R Negro Rojo) 
+        * genera las tiradas y detecta si ha salido poker 
+        */
+
+        
+
+    }
+    
+}
---
+++ Tema3Prog/EjerciciosArrays/EjerciciosClase3.java
@@ -0,0 +1,14 @@
+package Tema3Prog.EjerciciosArrays;
+
+public class EjerciciosClase3 {
+
+    public static void main(String[] args) {
+        
+
+        /*3. Crea un array con 20 palabras en español. Crea otro array con los mismas palabras en ingles
+         * Haz una FUNCIÓN que se le pase una palabra en ingles, y te devuelva la traducción en Español.
+         */
+
+    }
+    
+}
