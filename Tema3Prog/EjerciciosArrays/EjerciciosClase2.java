e841ba1a06983f0d9073ea0865cf5ac060a5cd8e
alxrgdriguez <sralexred@gmail.com>
2023-11-10 18:01:26 +0100

    EjerciciosClase

--- Tema3Prog/EjerciciosArrays/EjerciciosClase2.java
+++ Tema3Prog/EjerciciosArrays/EjerciciosClase2.java
@@ -1,6 +1,4 @@
 package Tema3Prog.EjerciciosArrays;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 public class EjerciciosClase2 {
--- Tema3Prog/EjerciciosArrays/EjerciciosClase3.java
+++ Tema3Prog/EjerciciosArrays/EjerciciosClase3.java
@@ -2,12 +2,28 @@ package Tema3Prog.EjerciciosArrays;
 
 public class EjerciciosClase3 {
 
+    
+
+
+
     public static void main(String[] args) {
         
 
         /*3. Crea un array con 20 palabras en español. Crea otro array con los mismas palabras en ingles
          * Haz una FUNCIÓN que se le pase una palabra en ingles, y te devuelva la traducción en Español.
          */
+
+        String palabrasEspanol = "";
+
+        String palabrasEspaniol[] = {"Casa", "Movil", "Elegir", "Perro", "Gato", "Caballo", "Cocina","Uno","Dos","Tres","Cuatro","Cinco",
+        "Seis", "Siete", "Ocho", "Nueve", "Diez", "Ordenador", "Disco","Hamster"};
+
+        String palabrasIngles[] = {"House", "mobile", "Choose", "Dog", "Cat", "Hourse", "Kitchen","One","Two","Three","Four","Five",
+        "Six", "Seven", "Eight", "Nine", "Ten", "Computer", "Disk","Hamster"};
+
+
+
+
 
     }
     
