adc86afbfad6032601f09fc5b17fc38983049018
alxrgdriguez <sralexred@gmail.com>
2023-11-14 21:34:35 +0100

    Ejercicio5

---
+++ Tema3Prog/EjerciciosUT3/Ejercicio5.java
@@ -0,0 +1,16 @@
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        
+        /*Leer 10 números enteros y a continuación mostrar la media de los números introducidos, el mayor valor y el
+        menor valor. Usar un array para almacenar los valores y sacar toda la información en una sola iteración del array.
+        Si puedes mejóralo implementando una función para cada tarea (media, mayor, menor), pasando el array. */
+
+        int[] numeros = new int[10];
+
+        
+
+
+    }
+    
+}
