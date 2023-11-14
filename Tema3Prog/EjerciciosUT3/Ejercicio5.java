58138ece85c97bebcfb909135604fb6c0319ecb6
alxrgdriguez <sralexred@gmail.com>
2023-11-14 21:50:32 +0100

    Ejercicio5

--- Tema3Prog/EjerciciosUT3/Ejercicio5.java
+++ Tema3Prog/EjerciciosUT3/Ejercicio5.java
@@ -1,4 +1,15 @@
+package Tema3Prog.EjerciciosUT3;
+
+import java.util.Arrays;
+
 public class Ejercicio5 {
+
+    public static int numaleatorio(int min, int max){
+
+        return (int) (Math.random() * (max - min + 1) + min);
+
+    }
+
 
     public static void main(String[] args) {
         
@@ -7,10 +18,31 @@ public class Ejercicio5 {
         Si puedes mejóralo implementando una función para cada tarea (media, mayor, menor), pasando el array. */
 
         int[] numeros = new int[10];
+        int mediaNumeros = 0;
+        int sumaNumeros = 0;
 
-        
+
+        for (int i = 0; i < numeros.length; i++){
+
+            numeros[i] = numaleatorio(1, 1);
+
+            //Calculamos la suma 
+
+            sumaNumeros += numeros[i];
+
+            //Calcular la media
+
+            mediaNumeros = sumaNumeros / numeros.length;
+
+        }
+           
+
+        System.out.println(Arrays.toString(numeros));
+        System.out.println("La suma de todas las posciones del array es de " + sumaNumeros);
+        System.out.println("La media de la suma es de: " + mediaNumeros);
 
 
     }
-    
+
 }
+ 
