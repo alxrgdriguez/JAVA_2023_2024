ac1c0b13df43aed3af99fb7adbe00b9853360ec8
alxrgdriguez <sralexred@gmail.com>
2023-11-17 14:03:34 +0100

    Ejercicio9

---
+++ Tema3Prog/EjerciciosUT3/Ejercicio9.java
@@ -0,0 +1,12 @@
+public class Ejercicio9 {
+
+    /**
+     * Ejercicio 9. Realizar un programa que lea los tiempos en los que de 10 corredores han acabado una carrera. El programa debe
+       determinar qué corredores tienen el primer, segundo y último puesto, así como cuál es el tiempo medio en que
+       se ha corrido la carrera. Pista: ordénalo antes
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
