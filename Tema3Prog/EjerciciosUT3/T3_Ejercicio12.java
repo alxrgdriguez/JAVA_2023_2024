35ea605c173c0498d1da84165cf587ca5aada31c
alxrgdriguez <sralexred@gmail.com>
2023-11-20 20:57:03 +0100

    T3_Ejercicio13

--- Tema3Prog/EjerciciosUT3/T3_Ejercicio12.java
+++ Tema3Prog/EjerciciosUT3/T3_Ejercicio12.java
@@ -26,24 +26,6 @@ public class T3_Ejercicio12 {
 
     }
 
-    public static void llenaCeros(int[][] matriz) {
-        for (int i = 0; i < matriz.length; i++) { //fila 0
-
-            for (int j = 0; j < matriz[i].length; j++) {
-
-                if (i == 0 || i == matriz.length-1 ||  j == 0 || j == matriz[i].length -1){
-
-                    matriz[i][j] = 0;
-                    System.out.print(matriz[i][j]+ " ");
-
-                }
-
-            }
-
-            System.out.println();
-        }
-
-    }
 
     public static void main(String[] args) {
         
