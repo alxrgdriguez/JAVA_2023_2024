ddbd9a00d43455e24092e6cc664aa6b05449d601
alxrgdriguez <sralexred@gmail.com>
2023-11-20 20:29:49 +0100

    Ejercicio12

---
+++ Tema3Prog/EjerciciosUT3/Ejercicio12.java
@@ -0,0 +1,38 @@
+public class Ejercicio12 {
+
+     /**
+     * Función para pintar por consola los valores de una matriz de enteros
+     * @param matriz
+     *      */
+    public static void pintarMatriz(int[][] matriz) {
+        for(int i=0; i<matriz.length; i++) {
+            
+            for(int j=0; j<matriz[0].length; j++) {
+                if(matriz[i][j] <= 9){
+                    System.out.print(" " + matriz[i][j] + " ");
+                }else{
+                    System.out.print(matriz[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static int numaleatorio(int min, int max){
+
+        return (int) (Math.random() * (max - min + 1) + min);
+
+    }
+
+    public static void main(String[] args) {
+        
+        /**
+         * Ejercicio 12. Crear una matriz “marco” de tamaño 8x6. Una matriz “marco” es aquella que todos sus elementos son 0 salvo los
+          de los bordes que deben ser 1. Mostrarla.
+         */
+
+
+
+    }
+    
+}
