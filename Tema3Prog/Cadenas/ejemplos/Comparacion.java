046fe30e36ef30402d99df3d59e0b274ad36a223
alxrgdriguez <sralexred@gmail.com>
2023-11-21 21:25:16 +0100

    Comparacion

---
+++ Tema3Prog/Cadenas/ejemplos/Comparacion.java
@@ -0,0 +1,14 @@
+public class Comparacion {
+    
+    public static void main(String[] args) {
+        
+        String nombre = "Zavier";
+        String nombre2 = "Savier";
+
+        if (nombre == nombre2) {
+            System.out.println("Son iguales");
+        }
+
+        System.out.println( nombre.compareTo(nombre2) );
+    }
+}
