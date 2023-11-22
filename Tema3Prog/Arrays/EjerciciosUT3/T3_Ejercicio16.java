df7090b8430d9e9ac0c3be0cc6698c4fc345c4ec
alxrgdriguez <sralexred@gmail.com>
2023-11-22 18:53:22 +0100

    T3_Ejercicio16

--- Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio16.java
+++ Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio16.java
@@ -1,11 +1,39 @@
 public class T3_Ejercicio16 {
 
+     /**
+     * Función para pintar por consola los valores de una matriz de enteros
+     * @param matriz
+     *      */
+    public static void pintarMatriz(int[][] matriz) {
+        for(int i=0; i<matriz.length; i++) {
+            
+            for(int j=0; j<matriz[0].length; j++) {
+                if(matriz[i][j] <= 9){
+                    System.out.print("0" + matriz[i][j] + " ");
+                }else{
+                    System.out.print(matriz[i][j] + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static int numaleatorio(int min, int max){
+
+        return (int) (Math.random() * (max - min + 1) + min);
+
+    }
+
     public static void main(String[] args) {
         
 
         /**
-         *Ejercicio 16. 16. Crea una matriz de 3x6 números enteros aleatorios no repetidos
+         *Ejercicio 16. Crea una matriz de 3x6 números enteros aleatorios no repetidos
          */
+
+        int[][] matriz = new int[3][6];
+
+
 
 
     }
