08dd3dd9766846ab5f1728d50c74f06b0aaca7db
alxrgdriguez <sralexred@gmail.com>
2023-11-22 18:48:38 +0100

    T3_Ejercicio16

---
+++ Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio16.java
@@ -0,0 +1,13 @@
+public class T3_Ejercicio16 {
+
+    public static void main(String[] args) {
+        
+
+        /**
+         *Ejercicio 16. 16. Crea una matriz de 3x6 números enteros aleatorios no repetidos
+         */
+
+
+    }
+    
+}
