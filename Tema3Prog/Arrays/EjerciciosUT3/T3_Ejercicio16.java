da8b14628168aedd20dc28fa320fbc89664f3dd1
alxrgdriguez <sralexred@gmail.com>
2023-11-23 15:50:48 +0100

    T3_Ejercicio16

--- Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio16.java
+++ Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio16.java
@@ -1,69 +1,60 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class T3_Ejercicio16 {
 
-        /**
-     * Función para pintar por consola los valores de una matriz de enteros
-     * @param matriz
-     *      */
+    public static void main(String[] args) {
+        // Crear una matriz de 3x6
+        int filas = 3;
+        int columnas = 6;
+        int[][] matriz = new int[filas][columnas];
+
+        // Llenar la matriz con números aleatorios no repetidos
+        llenarMatriz(matriz);
+
+        // Mostrar la matriz
+        System.out.println("-- MATRIZ --");
+        pintarMatriz(matriz);
+    }
+
+    // Función para llenar la matriz con números aleatorios no repetidos
+    public static void llenarMatriz(int[][] matriz) {
+        Set<Integer> numerosUtilizados = new HashSet<>();
+        int min = 1;
+        int max = 100;
+
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                int numeroAleatorio;
+                while (true) {
+                    numeroAleatorio = numAleatorio(min, max);
+                    if (!numerosUtilizados.contains(numeroAleatorio)) {
+                        break;
+                    }
+                }
+
+                matriz[i][j] = numeroAleatorio;
+                numerosUtilizados.add(numeroAleatorio);
+            }
+        }
+    }
+
+    // Función para generar un número aleatorio en un rango
+    public static int numAleatorio(int min, int max) {
+        return (int) (Math.random() * (max - min + 1) + min);
+    }
+
+    // Función para imprimir la matriz
     public static void pintarMatriz(int[][] matriz) {
-        for(int i=0; i<matriz.length; i++) {
-            
-            for(int j=0; j<matriz[0].length; j++) {
-                if(matriz[i][j] <= 9){
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if (matriz[i][j] <= 9) {
                     System.out.print("0" + matriz[i][j] + " ");
-                }else{
+                } else {
                     System.out.print(matriz[i][j] + " ");
                 }
             }
             System.out.println();
         }
     }
-
-   
-
-    public static int numaleatorio(int min, int max){
-
-        return (int) (Math.random() * (max - min + 1) + min);
-
-    }
-
-    public static void main(String[] args) {
-        
-
-        /**
-         *Ejercicio 16. Crea una matriz de 3x6 números enteros aleatorios no repetidos
-         */
-
-        int[][] matriz = new int[3][6];
-
-        for(int i = 0; i < matriz.length; i++){
-
-            for(int j = 0; j < matriz[i].length; j++){
-
-                matriz[i][j] = numaleatorio(10, 50);
-
-            }
-        }
-    
-          // Elimina los números repetidos
-          for (int i = 0; i < matriz.length; i++) {
-
-            for (int j = 0; j < matriz[i].length; j++) {
-
-                for (int k = 0; k < matriz[i].length; k++) {
-
-                    if (i != k && matriz[i][j] == matriz[k][j]) {
-
-                        matriz[i][j] = numaleatorio(1, 100);
-                    }
-                }
-            }
-        }
-        
-
-    //Pintamos la matriz rellenada con numeros aleatorios
-    System.out.println("\n-- MATRIZ --");
-    pintarMatriz(matriz);
-
-    }
-    
 }
