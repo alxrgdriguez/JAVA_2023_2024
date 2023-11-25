a76b23e5e0d64bbf4a30233f3ede6eaa0030c354
alxrgdriguez <sralexred@gmail.com>
2023-11-25 12:50:11 +0100

    Ejercicio10

--- Tema3Prog/Arrays/EjerciciosUT3/Ejercicio10.java
+++ Tema3Prog/Arrays/EjerciciosUT3/Ejercicio10.java
@@ -1,23 +1,16 @@
 public class Ejercicio10 {
 
-
-      /**
+     
+    /**
      * Función para pintar por consola los valores de una matriz de enteros
      * @param matriz
-     **/
-     public static void pintarMatriz(int[][] matriz){
-
-        for(int i = 0; i < matriz.length; i++){
-
-            for(int j = 0; j < matriz[j].length; i++){
-
-                if(matriz[i][j] <= 9){
-
-                    System.out.print("0" + matriz[i][j] + " ");
-                }else{
-                    System.out.print(matriz[i][j] + " ");
-                }
+     *      */
+    public static void pintarMatriz(int matriz[][]) {
+        for(int i=0; i<matriz.length; i++) {
+            for(int j=0; j<matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + " ");
             }
+            System.out.println();
         }
     }
 
@@ -26,6 +19,22 @@ public class Ejercicio10 {
         return (int) (Math.random() * (max - min + 1) + min);
     }
 
+
+    public static boolean siRepite(int[][] numLoteria, int numero){
+
+        for (int i = 0; i < numLoteria.length; i++){
+
+            for(int j = 0; j < numLoteria[i].length; j++){
+
+                if(numLoteria[i][j] == numero){
+
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
 
 
     public static void main(String[] args) {
@@ -36,7 +45,25 @@ public class Ejercicio10 {
          */
 
         int[][] numLoteria = new int[6][6];
+        int numero = 0;
 
+        for (int i = 0; i < numLoteria.length; i++) {
+            
+            for (int j = 0; j < numLoteria[i].length; j++) {
+
+                do {
+
+                    numero = numAleatorio(1, 49);
+                    
+                } while (siRepite(numLoteria, numero));
+
+                numLoteria[i][j] = numero;
+
+                
+            }
+        }
+
+        pintarMatriz(numLoteria);
 
     }
     
