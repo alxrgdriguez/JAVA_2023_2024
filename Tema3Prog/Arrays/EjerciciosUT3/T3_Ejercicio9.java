0bd72c32ec13815c73e63f53c9c9f69e83ae8234
alxrgdriguez <sralexred@gmail.com>
2023-11-25 14:08:14 +0100

    T3_Ejercicio9

--- Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio9.java
+++ Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio9.java
@@ -1,25 +1,38 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-
 public class T3_Ejercicio9 {
 
-  
+  public static double mediaCorredor(double[] tiempos){
 
-    /**
+    double media = 0.0;
+
+    for(int i = 0; i < tiempos.length; i++){
+
+      media += tiempos[i];
+
+    }
+    
+    media /= tiempos.length;
+
+    return media;
+  }
+
+    public static void main(String[] args) {
+        
+       /**
      * Ejercicio 9. Realizar un programa que lea los tiempos en los que de 10 corredores han acabado una carrera. El programa debe
        determinar qué corredores tienen el primer, segundo y último puesto, así como cuál es el tiempo medio en que
        se ha corrido la carrera. Pista: ordénalo antes
      */
-    public static void main(String[] args) {
-        
+
       Scanner sc = new Scanner(System.in);
 
         // Declarar el array para almacenar los tiempos de los corredores
         double[] tiempos = new double[10];
 
         // Leer los tiempos de los corredores
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < tiempos.length; i++) {
             System.out.print("Ingrese el tiempo del corredor " + (i + 1) + ": ");
             tiempos[i] = Double.parseDouble(sc.nextLine());
         }
@@ -33,7 +46,11 @@ public class T3_Ejercicio9 {
          System.out.println("Segundo puesto: Corredor 2 - Tiempo: " + tiempos[1] + " segundos");
          System.out.println("Último puesto: Corredor 10 - Tiempo: " + tiempos[9] + " segundos");
 
+         // Calcular y mostrar el tiempo medio
+        double tiempoMedio = mediaCorredor(tiempos);
+        System.out.println("\n El tiempo medio de la carrera es de: " + tiempoMedio + " segundos");
 
+        sc.close();
     }
     
 }
