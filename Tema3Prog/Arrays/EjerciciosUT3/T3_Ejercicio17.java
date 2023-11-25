a23397d33295ab1c85138728d25dc595fcbb88e1
alxrgdriguez <sralexred@gmail.com>
2023-11-25 14:09:05 +0100

    T3_Ejercicio17

--- Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio17.java
+++ Tema3Prog/Arrays/EjerciciosUT3/T3_Ejercicio17.java
@@ -2,6 +2,15 @@ public class T3_Ejercicio17 {
 
     public static void main(String[] args) {
         
+
+        /**Ordenar matrices. Crea una matriz de enteros de dos dimensiones de 50x50 elementos, rellénala con números
+        aleatorios entre 1 y 500. A continuación, realiza dos funciones:
+        a. ordenaFilas(matriz): que ordene la matriz por filas, cada fila de la matriz quedará ordenada de menor a
+        mayor.
+        b. ordenaColumnas(matriz): que ordene la matriz por columnas, cada columna quedará ordenada de
+        menor a mayor, independientemente de las demás.
+        */
+
     }
     
 }
