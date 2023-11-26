09c9d1b9385b0532b0b37d453d59d55c35978cde
alxrgdriguez <sralexred@gmail.com>
2023-11-26 20:25:11 +0100

    T3_String4

---
+++ Tema3Prog/Cadenas/T3_EjerString/T3_String4.java
@@ -0,0 +1,20 @@
+package Tema3Prog.Cadenas.T3_EjerString;
+
+public class T3_String4 {
+
+    public static void main(String[] args) {
+        
+
+        /**
+         * Ejercicio 4. Crea un programa en Java que solicite al usuario dos cadenas de caracteres y que devuelva la primera
+           cadena, pero transformando en mayúsculas la parte que coincide con la segunda cadena introducida.
+           Por ejemplo, si se introducen las cadenas “Esta es mi amiga Ana” y “amiga” devolverá “Esta es mi
+           AMIGA Ana”.
+         */
+
+
+
+
+    }
+    
+}
