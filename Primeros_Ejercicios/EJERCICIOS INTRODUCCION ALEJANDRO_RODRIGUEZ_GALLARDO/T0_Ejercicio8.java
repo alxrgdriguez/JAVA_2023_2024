638dc647dffc53f4fd84b1c786da91d0e5899797
alxrgdriguez <sralexred@gmail.com>
2023-11-26 12:21:50 +0100

    T0_Ejercicios

--- Primeros_Ejercicios/EJERCICIOS INTRODUCCION ALEJANDRO_RODRIGUEZ_GALLARDO/T0_Ejercicio8.java
+++ Primeros_Ejercicios/EJERCICIOS INTRODUCCION ALEJANDRO_RODRIGUEZ_GALLARDO/T0_Ejercicio8.java
@@ -6,7 +6,7 @@ class T0_Ejercicio8{
         // 8. Modifica el siguiente programa para hacer que compile y funcione:
 
         //No funciona porque a n3 le falta otorgarle un valor, porque no se puedo sumar algo a la nada
-        int n2=30, suma=0, n3;
+        int n2=30, suma=0, n3 = 10;
         suma=n1+n2;
         System.out.println("LA SUMA ES: " + suma);
         suma=suma+n3;
