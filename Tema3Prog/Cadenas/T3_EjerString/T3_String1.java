474e2479f6dbc92bf2f8f139e7e8d1dc81e3b329
alxrgdriguez <sralexred@gmail.com>
2023-11-26 18:56:02 +0100

    T3_String

--- Tema3Prog/Cadenas/T3_EjerString/T3_String1.java
+++ Tema3Prog/Cadenas/T3_EjerString/T3_String1.java
@@ -1,4 +1,9 @@
 package Tema3Prog.Cadenas.T3_EjerString;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
 class T3_String1{
 
     public static void main(String[] args) {
@@ -8,7 +13,37 @@ class T3_String1{
            devuelva esta cadena invertida. Haz dos versiones, una con String y otra con StringBuffer
          */
 
-        
-        
+        //Declaramos nuestro Scanner
+        Scanner sc = new Scanner(System.in);
+
+        //Declaramos nuestra variable tipo cadena
+        String cadena = "";
+        String temp = "";
+        //Pedimos al usuario que nos introduzca la cadena pedida por teclado
+        System.out.println("Introduce una cadena de caracteres: ");
+        cadena = sc.nextLine();
+
+      //Recorremos la cadena de la ultima posición hacia la primera para ir añadiendo a la variable temp los carácteres uno por uno
+        for(int i = cadena.length()-1; i >= 0; i--){
+          temp += cadena.charAt(i);
+        }
+
+        System.out.println(temp);
+
+
+
+        //Chuletas
+
+        /*int numeros = 234;
+
+        //Convertir un entero a cadena
+        String numerosCadena = String.valueOf(numeros);
+
+        //....
+
+        //Convertir una cadena a entero
+        numeros = Integer.parseInt(numerosCadena);*/
+
+
     }
 }
---
+++ Tema3Prog/Cadenas/T3_EjerString/T3_String1_StringBuffer.java
@@ -0,0 +1,9 @@
+package Tema3Prog.Cadenas.T3_EjerString;
+
+public class T3_String1_StringBuffer {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
