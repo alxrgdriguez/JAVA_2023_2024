69f465103c7ed2de836c89555a025eff25a1aa54
alxrgdriguez <sralexred@gmail.com>
2023-11-29 20:50:50 +0100

    T3_String11

--- Tema3Prog/Cadenas/T3_EjerString/T3_String11.java
+++ Tema3Prog/Cadenas/T3_EjerString/T3_String11.java
@@ -13,9 +13,11 @@ public class T3_String11 {
         String resultado = cadena.trim();
                 
         System.out.println("**"+cadena+"**");
+        System.out.println();
         System.out.println("**"+resultado+"**");
               
         // Elimina todos los espacios, pero no los tabuladores ni retornos
+        System.out.println();
         System.out.println(resultado.replace(" ",""));
     }
     
