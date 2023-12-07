0e99480e50a3b137c44fbd8b4897437fc3356194
alxrgdriguez <sralexred@gmail.com>
2023-12-07 12:31:09 +0100

    dwadw

---
+++ hola.java
@@ -0,0 +1,17 @@
+public class hola {
+
+
+    public static void main(String[] args) {
+        
+        int num1 = 10;
+        int num2 = 20;
+        int suma = 0;
+
+        suma = num1 + num2;
+
+        System.out.println(suma);
+
+
+    }
+    
+}
