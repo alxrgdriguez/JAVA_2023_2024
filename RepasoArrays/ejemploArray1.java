9cb5a2b03c9dd00893c1e0db50131ce1d1c6241b
alxrgdriguez <sralexred@gmail.com>
2023-12-07 12:53:38 +0100

    ejemplo

---
+++ RepasoArrays/ejemploArray1.java
@@ -0,0 +1,18 @@
+package RepasoArrays;
+
+public class ejemploArray1 {
+
+    public static int numaleatorio(int min, int max){
+
+        return (int) (Math.random() * (max - min + 1) + min);
+
+    }
+    public static void main(String[] args) {
+        
+        
+
+
+
+    }
+    
+}
