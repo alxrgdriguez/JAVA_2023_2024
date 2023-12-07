64a992fc3f15e08060fdcb8e86e8779ec98ee0fa
alxrgdriguez <sralexred@gmail.com>
2023-12-07 18:08:44 +0100

    jj

--- Ejercicios_antes_Examen/laberinto.java
+++ Ejercicios_antes_Examen/laberinto.java
@@ -19,7 +19,7 @@ public class laberinto {
         - Genera una bomba, será una casilla aleatoria del tablero interior, no de los bordes. Ahí
         habrá una bomba oculta, solo se sabrá por sus coordenadas, si pasas por esa
         coordenada sin saberlo explotará y perderás.
-        
+
         Jugar:
         - El jugador aparece pintado con una “@” en la casilla (0,0).
         - Implementa una función pintar que pinte el tablero tras cada movimiento.
@@ -35,8 +35,9 @@ public class laberinto {
         - Si consigues llegar al carácter marcado con “#” habrás ganado. Muestra un mensaje
         indicándolo y también indica el número de pasos que has dado desde el inicio hasta
         encontrar la salida.
-
          */
+
+         
 
 
     }
