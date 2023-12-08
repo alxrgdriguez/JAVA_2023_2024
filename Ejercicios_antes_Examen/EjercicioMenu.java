473c48cb9d985ee33763d5e3eeb3a9b6719ebea1
alxrgdriguez <sralexred@gmail.com>
2023-12-08 12:03:09 +0100

    menu

---
+++ Ejercicios_antes_Examen/EjercicioMenu.java
@@ -0,0 +1,23 @@
+package Ejercicios_antes_Examen;
+
+public class EjercicioMenu {
+
+    public static void main(String[] args) {
+        
+        /**
+         *
+        Menú
+        Realiza un programa que pida un número por teclado. Luego muestra un menú con las
+        siguientes opciones:
+        1. Muestra la suma de cada uno de sus dígitos
+        2. Que diga si es primo o no
+        3. La longitud de una circunferencia cuyo radio fuera ese número
+        4. Salir
+        Para cada una de las opciones deberás llamar a una función que calcule lo que se pide y
+        devuelva el resultado.
+         */
+
+
+    }
+    
+}
