841d8cd5098d8964747ced94878ad303f3a676b1
alxrgdriguez <sralexred@gmail.com>
2023-12-08 15:23:15 +0100

    cubitus

--- Ejercicios_antes_Examen/EjercicioCubitusHumerus1.java
+++ Ejercicios_antes_Examen/EjercicioCubitusHumerus1.java
@@ -4,9 +4,26 @@ package Ejercicios_antes_Examen;
  * EjercicioCubitusHumerus1
  */
 public class EjercicioCubitusHumerus1 {
+
+    public static int numaleatorio(int min, int max){
+
+        return (int) (Math.random() * (max - min + 1) + min);
+
+    }
     public static void main(String[] args) {
         
+        /**
+         * Cubitus y Humerus 1
+           Marcus Cubitus y Julius Humerus son dos legionarios que se entretienen en sus ratos
+           libres jugando a juegos de dados, el pasatiempo favorito de los antiguos romanos. Un
+           juego que pudieron haber jugado estos amigos consiste en tirar cada jugador cinco dados.
+           La puntuación es la suma total de puntos, sin contar los valores que coincidan con el
+           mínimo o el máximo obtenido. El jugador que saca más puntos gana la partida. Escriba un
+           programa que simule varias partidas seguidas de este juego de dados.
+        */
+
         
+
 
     }
 
