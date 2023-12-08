8ef82b38f9f1e02644508dcda6c0f89d15c147c2
alxrgdriguez <sralexred@gmail.com>
2023-12-08 12:54:20 +0100

    cubitusHumerus1

---
+++ Ejercicios_antes_Examen/EjercicioCubitusHumerus1.java
@@ -0,0 +1,14 @@
+package Ejercicios_antes_Examen;
+
+/**
+ * EjercicioCubitusHumerus1
+ */
+public class EjercicioCubitusHumerus1 {
+    public static void main(String[] args) {
+        
+        
+
+    }
+
+    
+}
