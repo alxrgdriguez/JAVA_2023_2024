949c5540e5eeefc4c46f390d2e7b53d600718c5b
alxrgdriguez <sralexred@gmail.com>
2023-12-08 16:01:01 +0100

    cubitusHumerus1

--- Ejercicios_antes_Examen/EjercicioCubitusHumerus1.java
+++ Ejercicios_antes_Examen/EjercicioCubitusHumerus1.java
@@ -1,5 +1,7 @@
 package Ejercicios_antes_Examen;
 
+import java.util.Scanner;
+
 /**
  * EjercicioCubitusHumerus1
  */
@@ -22,10 +24,43 @@ public class EjercicioCubitusHumerus1 {
            programa que simule varias partidas seguidas de este juego de dados.
         */
 
+        Scanner sc = new Scanner(System.in);
+
+        int numTurnos = 0;
+        int tiradaJ1_1 = 0, tiradaJ1_2 = 0, tiradaJ1_3 = 0, tiradaJ1_4 = 0, tiradaJ1_5 = 0;
+        int tiradaJ2_1 = 0, tiradaJ2_2 = 0, tiradaJ2_3 = 0, tiradaJ2_4 = 0, tiradaJ2_5 = 0;
+
+        System.out.println("Introduce el numero de turnos que deseas jugar: ");
+        numTurnos = Integer.parseInt(sc.nextLine());
+
+
+        for (int i = 1; i < numTurnos; i++){
+
+            System.out.println("\n--------");
+            System.out.println("Turno " + i );
+            System.out.println("--------");
+            System.out.println();
+
+            //Tiradas del jugador 1 Cubitus
+
+            tiradaJ1_1 = numaleatorio(1, 6);
+            tiradaJ1_2 = numaleatorio(1, 6);
+            tiradaJ1_3 = numaleatorio(1, 6);
+            tiradaJ1_4 = numaleatorio(1, 6);
+            tiradaJ1_5 = numaleatorio(1, 6);
+            System.out.println("Tiradas Cubitus " + " | " + tiradaJ1_1 + " | " + tiradaJ1_2 + " | " + tiradaJ1_3 +  " | " + tiradaJ1_4 + " | "  + tiradaJ1_5 + " |");
         
+            //Tiradas del jugador 2 Humerus
 
-
+            tiradaJ2_1 = numaleatorio(1, 6);
+            tiradaJ2_2 = numaleatorio(1, 6);
+            tiradaJ2_3 = numaleatorio(1, 6);
+            tiradaJ2_4 = numaleatorio(1, 6);
+            tiradaJ2_5 = numaleatorio(1, 6);
+            System.out.println("Tiradas Humerus " + " | " + tiradaJ2_1 + " | " + tiradaJ2_2 + " | " + tiradaJ2_3 +  " | " + tiradaJ2_4 + " | "  + tiradaJ2_5 + " |");
+       
+        }
+    
     }
 
-    
 }
