477abd449ddd2d3c8945016c1972216375f30a69
alxrgdriguez <sralexred@gmail.com>
2023-12-09 12:55:59 +0100

    cadenas

---
+++ Ejercicios_antes_Examen/EjercicioCadenas.java
@@ -0,0 +1,20 @@
+package Ejercicios_antes_Examen;
+
+public class EjercicioCadenas {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Implementa un programa que pida al usuario que introduzca una frase, y a
+           continuación que introduzca dos cadenas. El programa debe mostrar por pantalla la frase
+           introducida en primer lugar, en la que se hayan sustituido todas las apariciones de la
+           primera cadena por la segunda cadena. Por ejemplo: si la frase introducida es “Quiero ir a
+           vivir cerca del Guadalquivir”, y las cadenas introducidas a continuación son “ir” e “IRR”, al
+           reemplazar la primera cadena por la segunda obtendríamos como resultado la
+           frase“Quiero IRR a vivIRR cerca del GuadalquivIRR”
+         */
+
+
+    }
+    
+}
