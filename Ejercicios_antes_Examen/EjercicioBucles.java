cbd6250a967d7614fce77083b81874c24d4e695f
alxrgdriguez <sralexred@gmail.com>
2023-12-09 13:49:13 +0100

    bucles

---
+++ Ejercicios_antes_Examen/EjercicioBucles.java
@@ -0,0 +1,24 @@
+package Ejercicios_antes_Examen;
+
+public class EjercicioBucles {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Escribe un programa que utilizando bucles muestre la siguiente figura por
+        pantalla
+        
+                *
+               ***
+              *****
+             *******
+              *****
+               ***
+                *
+        */
+
+        
+
+    }
+    
+}
