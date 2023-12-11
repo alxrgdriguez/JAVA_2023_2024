76b8f8eb10c7c7c7ea53f847e232bb5bdc5e00a7
alxrgdriguez <sralexred@gmail.com>
2023-12-11 18:22:33 +0100

    EjerciciosE

---
+++ Examen1ErTrimestre/Ejercicio1.java
@@ -0,0 +1,273 @@
+package Examen1ErTrimestre;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    /**
+     * Función que genera un número aleatorio entre un valor mínimo y máximo
+     * @param min
+     * @param max
+     * @return número aleatorio entre min y max, incluidos
+     */
+    public static int generarNumero(int min, int max)  {
+        return  (int) ((Math.random() * (max - min + 1) + min)) ;
+    }
+
+    /**
+     * Función para pintar por consola los valores de una matriz de enteros
+     * @param matriz
+     *      */
+    public static void pintarMatriz(int matriz[][]) {
+        for(int i=0; i<matriz.length; i++) {
+            for(int j=0; j<matriz[0].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static int sumarDiagonalP(int[][] matriz){
+
+        int resultado = 0;
+
+        for(int i = 0; i < matriz.length; i++){
+            resultado += matriz[i][i];
+
+        }
+
+        return resultado;
+
+    }
+
+
+    public static int sumarDiagonalI(int[][] matriz){
+
+        int resultado = 0;
+
+        for (int i = 0; i < matriz[0].length; i++) {
+
+            resultado += matriz[i][i];
+            
+        }
+
+        return resultado;
+
+    }
+
+    public static int sumarColumna(int[][] matriz, int columna){
+
+        int resultado = 0;
+
+        for(int i = 0; i < matriz[0].length; i++){
+
+            resultado += matriz[i][columna];
+        }
+
+        return resultado;
+
+    }
+
+    public static int maximoMatriz(int matriz[][]){
+
+        int maximo = Integer.MIN_VALUE;
+
+        for(int i = 0; i < matriz.length; i++){
+
+            for(int j = 0; j < matriz[0].length; j++){
+
+                //Decimos cual es el maximo
+
+                if(matriz[i][j] > maximo){
+
+                    maximo = matriz[i][j];
+                }
+            }
+        }
+
+        return maximo;
+
+    }
+
+
+    public static void matriztranspuesta(int[][] matriz){
+
+        for (int i = 0; i < matriz[0].length; i++) {
+
+            for (int j = 0; j < matriz.length; j++) {
+             
+                matriz[i][j] = matriz[j][i];
+            }
+            
+        }
+
+        System.out.println(Arrays.toString(matriz));
+
+    }
+
+ 
+
+
+    public static void main(String[] args) {
+        
+        /**
+         * Ejercicio 1. Nos piden crear una matriz 20×20 de números enteros que inicialmente
+            rellenamos de valores aleatorios (1-100), nos piden hacer un menú con estas opciones:
+
+            1. Intercambiar fila. Te pedirá dos números de fila e intercambiará los valores de la
+            primera por los valores de la segunda.
+            2. Suma de una columna que se pedirá al usuario (controlar que elija una correcta)
+            3. Sumar la diagonal principal y la diagonal inversa, y mostrar las dos sumas
+            4. Pintar las coordenadas i,j donde se encuentra el mayor elemento de la matriz.
+            5. Pinta la matriz traspuesta
+            6. Muestra la matriz con las filas ordenadas de mayor a menor
+            7. Salir
+         */
+
+        Scanner sc = new Scanner(System.in);
+
+        int[][] matriz = new int[20][20];
+        int opcion = 0;
+
+
+        //Rellenamos el tablero
+        for (int i = 0; i < matriz.length; i++) {
+            
+            for (int j = 0; j < matriz[i].length; j++) {
+
+                matriz[i][j] = generarNumero(1, 100);
+            }
+        }
+
+        //Pintamos la matriz generada con numeros aleatorios
+        System.out.println("---- MATRIZ ----");
+        pintarMatriz(matriz);
+        
+
+
+        do {
+        System.out.println("\n---- MENU ----");
+        System.out.println("1. Intercambiar fila. Te pedirá dos números de fila e intercambiará los valores de la" + 
+        "primera por los valores de la segunda");
+        System.out.println("2. Suma de una columna que se pedirá al usuario (controlar que elija una correcta)");
+        System.out.println("3. Sumar la diagonal principal y la diagonal inversa, y mostrar las dos sumas");
+        System.out.println("4. Pintar las coordenadas i,j donde se encuentra el mayor elemento de la matriz");
+        System.out.println("5. Pinta la matriz traspuesta");
+        System.out.println("6. Muestra la matriz con las filas ordenadas de mayor a menor");
+        System.out.println("7. Salir");
+        System.out.print("Introduce una opcion: ");
+
+        try {
+
+            opcion = Integer.parseInt(sc.nextLine());
+            
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            System.out.println("La opcion debe ser un numero valido");
+        }
+
+        switch (opcion) {
+            case 1:
+                int num1 = 0;
+                int num2 = 0;
+
+
+                do {
+
+                    System.out.println("Introduce el primer numero de fila que desees (0 - 19): ");
+
+                    try {
+                        num1 = Integer.parseInt(sc.nextLine());
+                }   catch (Exception e) {
+                        System.out.println(e.getMessage());
+                }
+                } while (num1 < 0 || num1 > 100);
+                
+
+                do {
+
+                    System.out.println("Introduce el segundo numero de fila que desees (0 - 19): ");
+
+                    try {
+                        num2 = Integer.parseInt(sc.nextLine());
+                }   catch (Exception e) {
+                        System.out.println(e.getMessage());
+                }
+                } while (num2 < 0 || num2 > 100);
+
+                break;
+
+            case 2:
+                int columna = 0;
+                System.out.print("Introduce la posicion de la colunmna: ");
+
+                if (columna < 0 && columna > 100) {
+                    
+                    System.out.println("Fuera de rango");
+                }
+
+            
+                try {
+
+                    columna = Integer.parseInt(sc.nextLine());
+                    
+                } catch (Exception e) {
+                    System.out.println("Debes introducir una columna VALIDA");
+                }
+
+                //LLamamos a la función para sumar la columna
+                System.out.println("La suma de la " + columna + " es: ");
+                System.out.println(sumarColumna(matriz, columna));
+            
+                break;
+
+            case 3:
+
+                //Llamamos a la funcion de sumar diagonal principal para mostrar la suma
+                System.out.println("La suma de la diagonal principal es: ");
+                System.out.println(sumarDiagonalP(matriz));
+
+                //Llamamos a la funcion de sumar diagonal inversa para mostrar la suma
+                System.out.println("\nLa suma de la diagonal inversa es: ");
+                System.out.println(sumarDiagonalI(matriz));
+                break;
+
+            case 4:
+            
+                System.out.println("El numero maximo de la matriz es: " + maximoMatriz(matriz));
+                break;
+
+            case 5:
+
+                matriztranspuesta(matriz);
+            
+                break;
+
+            case 6:
+
+                //System.out.println("Fila ordenada de mayor a menor: " + filaordenada(matriz));
+            
+                break;
+
+            case 7:
+
+                System.out.println("Adios");
+            
+                break;
+        
+            default:
+
+                System.out.println("Opcion no valida");
+                break;
+        }
+            
+        } while (opcion != 7);
+    
+
+
+
+    }
+    
+}
---
+++ Examen1ErTrimestre/Ejercicio2.java
@@ -0,0 +1,23 @@
+package Examen1ErTrimestre;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Ejercicio 2. Craps. Es un juego de dados muy facil, y consiste en arrojar la suma de 2 dados,
+        y su suma es 7 u 11 ganas la partida. En cambio, si sacas un 2, 3 o 12 pierdes. Con cualquier
+        otro numero que saques, debes guardar ese número y tirar los dados repetidamente hasta
+        volver a sacar ese dado (ganas) o sacar el 7 (pierdes). Realiza el juego para dos jugadores
+        simulando las tiradas de forma aleatoria y que jueguen 10 partidas, indica cuántas partidas
+        gana jugador1 y cuántas jugador2. Ahora simula 1000 partidas e indica las partidas que
+        gana cada uno.
+         */
+
+         
+
+
+
+    }
+    
+}
