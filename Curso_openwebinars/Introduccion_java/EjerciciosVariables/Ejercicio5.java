5f5a0bcd8d0da9cf2e1d12ab7e7d34c558380065
alxrgdriguez <sralexred@gmail.com>
2023-12-27 09:25:17 +0100

    Ejercicio5

---
+++ Curso_openwebinars/Introduccion_java/EjerciciosVariables/Ejercicio5.java
@@ -0,0 +1,18 @@
+package Curso_openwebinars.Introduccion_java.EjerciciosVariables;
+
+public class Ejercicio5 {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Escribir un programa que, dada una cantidad de euros, 
+         * nos indique cuál es su valor en dólares americanos. Además de la variable para la
+         * cantidad de euros, se debe declarar otra para el cambio (el valor de un dólar en euros) 
+         * y para el resultado final. Muestra el resultado por consola.
+         */
+
+
+
+    }
+    
+}
