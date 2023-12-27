c0fc8427678c69232811a27b103a77c43c949a04
alxrgdriguez <sralexred@gmail.com>
2023-12-27 10:21:34 +0100

    Ejercicio10

---
+++ Curso_openwebinars/Introduccion_java/EjerciciosVariables/Ejercicio10.java
@@ -0,0 +1,12 @@
+package Curso_openwebinars.Introduccion_java.EjerciciosVariables;
+
+public class Ejercicio10 {
+
+    public static void main(String[] args) {
+        
+        
+
+
+    }
+    
+}
