d6bb6b1114bb2c35204640300ae8925ab20d39d9
alxrgdriguez <sralexred@gmail.com>
2023-12-27 09:51:12 +0100

    Ejercicio7

---
+++ Curso_openwebinars/Introduccion_java/EjerciciosVariables/Ejercicio7.java
@@ -0,0 +1,16 @@
+package Curso_openwebinars.Introduccion_java.EjerciciosVariables;
+
+public class Ejercicio7 {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Escribe un programa que lea dos números enteros por teclado, y realice las operaciones básicas de una 
+         * calculadora con ellos: suma, resta, multiplicación, división y resto. 
+         * Debe mostrar todos los resultados por consola.
+         */
+
+
+    }
+    
+}
