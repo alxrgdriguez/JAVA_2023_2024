b00aac20b1b12a37482421257cb9036bab677022
alxrgdriguez <sralexred@gmail.com>
2023-12-27 09:17:21 +0100

    Ejercicio4

---
+++ Curso_openwebinars/Introduccion_java/EjerciciosVariables/Ejercicio4.java
@@ -0,0 +1,18 @@
+package Curso_openwebinars.Introduccion_java.EjerciciosVariables;
+
+public class Ejercicio4 {
+
+    public static void main(String[] args) {
+        
+        /**
+         * Escribir un programa que calcule el perímetro y el área de un círculo y la muestre por consola. 
+         * El `radio` del mismo lo puedes proporcionar 
+         * como un valor inicial de la variable correspondiente.
+         */
+
+         
+
+
+    }
+    
+}
