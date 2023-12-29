6cafb259251a448023627ac17326cb1edc373623
alxrgdriguez <sralexred@gmail.com>
2023-12-29 20:29:49 +0100

    Ejercicio2

---
+++ Curso_openwebinars/Introduccion_java/Ejercicios_estrecturas_control/Ejercicio2.java
@@ -0,0 +1,12 @@
+package Curso_openwebinars.Introduccion_java.Ejercicios_estrecturas_control;
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        
+        
+
+
+    }
+    
+}
