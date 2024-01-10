93249e092a99b876e8547258d02fda9490320c69
alxrgdriguez <sralexred@gmail.com>
2024-01-10 19:01:05 +0100

    Guardado

---
+++ IntelliJ_2TR/Ejemplos_BasicosPOO/TestJugador.java
@@ -0,0 +1,11 @@
+package IntelliJ_2TR.Ejemplos_BasicosPOO;
+
+public class TestJugador {
+
+    public static void main(String[] args) {
+
+        Jugador j1 = new Jugador("Aurelius", "aure122","dwda@gmail.com",18,20,"Minecraft");
+        System.out.println(j1);
+
+    }
+}
