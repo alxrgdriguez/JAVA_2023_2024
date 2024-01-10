3a95371bba12ee1201e1d87be32a22d46cf62bc2
alxrgdriguez <sralexred@gmail.com>
2024-01-10 17:48:47 +0100

    Guardado

---
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio3/MainPajaro.java
@@ -0,0 +1,13 @@
+package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio3;
+
+public class MainPajaro {
+
+    public static void main(String[] args) {
+
+        Pajaro pajaro1 = new Pajaro();
+
+
+    }
+
+
+}
