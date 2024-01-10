d5fc0aed3405eb2bf3164c374bb4ec303858276f
alxrgdriguez <sralexred@gmail.com>
2024-01-10 16:57:34 +0100

    Guardado

---
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio2/Coche.java
@@ -0,0 +1,29 @@
+package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio2;
+
+public class Coche {
+
+    //Propiedades
+    private int velocidad;
+
+    //Constructor
+    Coche () { velocidad = 0;}
+
+    //Metodos
+
+    //Velocidad actual
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    //Acelerar
+    public void acelera(int mas){
+
+        this.velocidad += mas;
+    }
+
+    //Frenar
+    public void frenar(int menos){
+
+        this.velocidad -=menos;
+    }
+}
---
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio2/MainCoche.java
@@ -0,0 +1,10 @@
+package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio2;
+
+public class MainCoche {
+
+    public static void main(String[] args) {
+
+        Coche co1 = new Coche();
+
+    }
+}
