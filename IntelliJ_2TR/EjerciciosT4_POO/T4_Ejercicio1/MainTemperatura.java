636933674a679e71847ad1f29dd583a4d944c575
alxrgdriguez <sralexred@gmail.com>
2024-01-10 17:21:19 +0100

    Guardado

---
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio1/MainTemperatura.java
@@ -0,0 +1,10 @@
+package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio1;
+
+public class MainTemperatura {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
---
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio1/Temperatura.java
@@ -0,0 +1,18 @@
+package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio1;
+
+public class Temperatura {
+
+  //Metodos
+
+    public double celsiusToFarenheit(double celsius){
+
+        return (1.8) * celsius + 32;
+    }
+
+    public  double farenheitToCelsius(double farenheit){
+
+        return (farenheit - 32) / 1.8;
+    }
+
+
+}
