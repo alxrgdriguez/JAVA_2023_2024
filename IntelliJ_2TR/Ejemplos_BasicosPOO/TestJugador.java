e089732eb9dcc3f17efcab191247198dcb5d0eee
alxrgdriguez <sralexred@gmail.com>
2024-01-10 19:02:24 +0100

    Guardado

--- IntelliJ_2TR/Ejemplos_BasicosPOO/TestJugador.java
+++ IntelliJ_2TR/Ejemplos_BasicosPOO/TestJugador.java
@@ -4,7 +4,9 @@ public class TestJugador {
 
     public static void main(String[] args) {
 
-        Jugador j1 = new Jugador("Aurelius", "aure122","dwda@gmail.com",18,20,"Minecraft");
+        Jugador j1 = new Jugador("Aurelius", "aure122","dwda@gmail.com",18,20,
+        "Minecraft");
+
         System.out.println(j1);
 
     }
