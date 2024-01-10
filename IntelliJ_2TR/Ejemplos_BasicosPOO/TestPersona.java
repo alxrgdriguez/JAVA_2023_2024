1e333de78b859dd557d8628bbfff77e62430f64a
alxrgdriguez <sralexred@gmail.com>
2024-01-10 18:53:01 +0100

    Guardado

---
+++ IntelliJ_2TR/Ejemplos_BasicosPOO/TestPersona.java
@@ -0,0 +1,13 @@
+package IntelliJ_2TR.Ejemplos_BasicosPOO;
+
+public class TestPersona {
+
+    public static void main(String[] args) {
+
+        Persona per1 = new Persona();
+
+        System.out.println(per1);
+
+
+    }
+}
