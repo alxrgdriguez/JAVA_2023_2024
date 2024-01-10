614d1f6800e78afd5a219d928b521bd7811590c5
alxrgdriguez <sralexred@gmail.com>
2024-01-10 17:24:15 +0100

    Guardado

--- IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio1/MainTemperatura.java
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio1/MainTemperatura.java
@@ -4,7 +4,8 @@ public class MainTemperatura {
 
     public static void main(String[] args) {
 
-
+        System.out.println("Celsius a Farenheit: "+new Temperatura().celsiusToFarenheit(10.5));
+        System.out.println("Farenheit a Celsius: "+new Temperatura().farenheitToCelsius(82.6));
 
     }
 }
