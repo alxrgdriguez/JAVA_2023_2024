382cb827a6ddaa11e6d10a1532125bbc977fdb3e
alxrgdriguez <sralexred@gmail.com>
2024-01-10 17:09:16 +0100

    Guardado

--- IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio2/MainCoche.java
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio2/MainCoche.java
@@ -6,5 +6,15 @@ public class MainCoche {
 
         Coche co1 = new Coche();
 
+        System.out.println(co1.getVelocidad());
+
+        co1.acelera(100);
+
+        System.out.println("El coche acelera a " + co1.getVelocidad() + " KM/H");
+
+        co1.frenar(20);
+
+        System.out.println("El coche ha frenado y ha pasado de 100 a " + co1.getVelocidad()+ " KM/H");
+
     }
 }
