da3c3817cc77b8f33d3dc938c701908ec663e1f2
alxrgdriguez <sralexred@gmail.com>
2024-01-12 17:59:58 +0100

    Guardado

---
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio7/Incidencia.java
@@ -0,0 +1,10 @@
+package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio7;
+
+public class Incidencia {
+
+    private int codigo;
+    private String estado;
+    private String problema;
+    private String solucion;
+    private static int pendientes;
+}
---
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio7/TestIncidencia.java
@@ -0,0 +1,8 @@
+package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio7;
+
+public class TestIncidencia {
+
+    public static void main(String[] args) {
+
+    }
+}
