0c5f3e44cc9366a993277c8f2ac91e8e30e7ba9e
alxrgdriguez <sralexred@gmail.com>
2024-01-12 17:55:52 +0100

    Guardado

--- IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio6/TestSteamJuego.java
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio6/TestSteamJuego.java
@@ -8,6 +8,11 @@ public class TestSteamJuego {
         SteamJuego juego2 = new SteamJuego("Phasmofobia","Terror",450.23,863,19.99,120);
         SteamJuego juego3 = new SteamJuego("Howards Legacy","Magia",2540.72,365,49.99,1000);
 
+        System.out.println("--- ORDENACION DE PUESTOS MAS VENDIDOS ---");
+        System.out.println("Howards Legacy --> " + juego3.getPuestosMasVendidos());
+        System.out.println("Counter Strike --> " + juego1.getPuestosMasVendidos());
+        System.out.println("Phasmofobia --> " + juego2.getPuestosMasVendidos());
+
 
         juego1.jugadoresDia(3254);
         juego2.jugadoresDia(225);
