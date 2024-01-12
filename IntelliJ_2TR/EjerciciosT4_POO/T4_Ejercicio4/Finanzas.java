82d3700a411ec58cee5b6741ef243b9c57085b68
alxrgdriguez <sralexred@gmail.com>
2024-01-12 16:29:14 +0100

    Guardado

--- IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio4/Finanzas.java
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio4/Finanzas.java
@@ -2,9 +2,39 @@ package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio4;
 
 public class Finanzas {
 
+    //Propiedades
     private double dola_euro;
 
+    //Constructor
 
+    public Finanzas() {
+        dola_euro = 1.36;
+    }
 
+    public Finanzas(double dola_euro) {
+        this.dola_euro = dola_euro;
+    }
 
+    //Métodos
+
+    public double dolaresToEuros(double dolares){
+
+        return this.dola_euro * dolares;
+
+    }
+
+    public double eurosToDolares(double euros){
+
+        return this.dola_euro * euros;
+    }
+
+    //Getters and Setters
+
+    public double getDola_euro() {
+        return dola_euro;
+    }
+
+    public void setDola_euro(double dola_euro) {
+        this.dola_euro = dola_euro;
+    }
 }
