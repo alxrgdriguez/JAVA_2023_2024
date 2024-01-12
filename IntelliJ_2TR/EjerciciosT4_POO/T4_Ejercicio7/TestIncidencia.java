eb3449da64f3bc619bd7a96f00e039d91f9ed773
alxrgdriguez <sralexred@gmail.com>
2024-01-12 19:27:39 +0100

    Temperatura

--- IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio7/TestIncidencia.java
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio7/TestIncidencia.java
@@ -4,5 +4,20 @@ public class TestIncidencia {
 
     public static void main(String[] args) {
 
+        System.out.println();
+        Incidencia incidencia1 = new Incidencia (1, "Abierta", "Fallo en la iluminación", "Cambiar bombilla pasillo A");
+        Incidencia incidencia2 = new Incidencia (2, "En proceso", "Cuenta de Steam", "Restablecer password");
+
+        System.out.println(incidencia1.toString());
+        System.out.println("Incidencias pendientes codigo 1: "  + Incidencia.getPendientes());
+
+        System.out.println(incidencia2.toString());
+        System.out.println("Incidencias pendientes codigo 2: " + Incidencia.getPendientes());
+
+        incidencia2.resuelve();
+        System.out.println(incidencia2.toString());
+        System.out.println("Incidencias resueltas: " + Incidencia.getPendientes());
+
+
     }
 }
---
+++ IntelliJ_2TR/EjerciciosT4_POO/T4_Ejercicio8/TestConsumo.java
@@ -0,0 +1,4 @@
+package IntelliJ_2TR.EjerciciosT4_POO.T4_Ejercicio8;
+
+public class TestConsumo {
+}
