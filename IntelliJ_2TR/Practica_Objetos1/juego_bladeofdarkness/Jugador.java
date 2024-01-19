47808eefe276cdac935056d8d57483e408a66e67
alxrgdriguez <sralexred@gmail.com>
2024-01-19 16:38:27 +0100

    Guardado

---
+++ IntelliJ_2TR/Practica_Objetos1/juego_bladeofdarkness/Jugador.java
@@ -0,0 +1,4 @@
+package IntelliJ_2TR.Practica_Objetos1.juego_bladeofdarkness;
+
+public class Jugador {
+}
---
+++ IntelliJ_2TR/Practica_Objetos1/juego_bladeofdarkness/TestJuego.java
@@ -0,0 +1,8 @@
+package IntelliJ_2TR.Practica_Objetos1.juego_bladeofdarkness;
+
+public class TestJuego {
+
+    public static void main(String[] args) {
+
+    }
+}
