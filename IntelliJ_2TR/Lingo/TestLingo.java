a8da1ac88f28a63dbc00350dbc36ae89fd81ecbf
alxrgdriguez <sralexred@gmail.com>
2024-01-24 18:53:46 +0100

    Guardado

---
+++ IntelliJ_2TR/Lingo/TestLingo.java
@@ -0,0 +1,26 @@
+package IntelliJ_2TR.Lingo;
+
+import java.util.Scanner;
+
+public class TestLingo {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Lingo juego = new Lingo();
+        System.out.println(juego.getPalabraAcertar());
+
+        while (true){
+
+            System.out.println("Introduce una palabra:");
+            String palabra = sc.nextLine();
+
+            //Comprobar el numero de aciertos
+
+            }
+        }
+
+
+
+    }
+}
