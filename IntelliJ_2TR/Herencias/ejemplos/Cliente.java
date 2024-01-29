552f8785c5f1c5f96cc5e05f1ba2b2bd499e33f3
alxrgdriguez <sralexred@gmail.com>
2024-01-29 19:26:26 +0100

    Guardado

---
+++ IntelliJ_2TR/Herencias/ejemplos/Cliente.java
@@ -0,0 +1,10 @@
+package IntelliJ_2TR.Herencias.ejemplos;
+
+public class Cliente {
+
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String ciudad;
+
+}
---
+++ IntelliJ_2TR/Herencias/ejemplos/Empleado.java
@@ -0,0 +1,9 @@
+package IntelliJ_2TR.Herencias.ejemplos;
+
+public class Empleado {
+
+    private String nombre;
+    private String telefono;
+    private String email;
+    private String ciudad;
+}
