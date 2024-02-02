7474852476e8e66b5ff709674dc6a3aea444045c
alxrgdriguez <sralexred@gmail.com>
2024-02-02 17:14:00 +0100

    Guardado

---
+++ Tema5Prog/Ejer_Practicar_POO/T5_Ejercicio6/Agenda.java
@@ -0,0 +1,71 @@
+package Tema5Prog.Ejer_Practicar_POO.T5_Ejercicio6;
+
+import java.util.ArrayList;
+
+public class Agenda {
+
+
+    //Propiedades
+    private ArrayList<Contacto> contactos;
+
+
+    //Cosntructor
+    public Agenda() {
+        this.contactos = new ArrayList<>(); //Incializamos el ArrayList en el constructor
+    }
+
+    //Getters
+
+    public ArrayList<Contacto> getContactos() {
+        return contactos;
+    }
+
+
+    //Metodos
+
+    /**
+     * Metodo para agregar un contacto a la agenda
+     * @param c
+     */
+    public void addContacto(Contacto c) {
+
+        contactos.add(c);
+    }
+
+    /**
+     * Metodo para eliminar un contacto de la agenda
+     * @param c
+     */
+
+    public void eliminarContacto(Contacto c) {
+
+        contactos.remove(c);
+    }
+
+    /**
+     * Metodo para verificar si existe un contacto en la agenda
+     * @param c
+     * @return
+     */
+    public boolean existeContacto(Contacto c) {
+        if (contactos.indexOf(c) < 0) {
+            System.out.println("El contacto no existe");
+            return false;
+
+        }else {
+            System.out.println("El contacto existe");
+            return true;
+        }
+    }
+
+    /*public boolean buscarContacto(Contacto c) {
+
+
+    }*/
+
+
+
+
+
+
+}
---
+++ Tema5Prog/Ejer_Practicar_POO/T5_Ejercicio6/Contacto.java
@@ -0,0 +1,60 @@
+package Tema5Prog.Ejer_Practicar_POO.T5_Ejercicio6;
+
+import java.util.Objects;
+
+public class Contacto {
+
+    //Propiedades
+    private String nombre;
+
+    private String telefono;
+
+
+    //Constructor
+    public Contacto(String nombre, String telefono) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    //Getters and Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    //equals
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Contacto contacto = (Contacto) o;
+        return Objects.equals(nombre, contacto.nombre);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre);
+    }
+
+    //Métodos
+
+    public String pintar() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("Nombre: " + nombre + "\n");
+        sb.append("Telefono: " + telefono + "\n");
+        return sb.toString();
+    }
+}
