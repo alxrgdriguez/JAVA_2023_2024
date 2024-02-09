54e0deb815ebfaa9315407478504cb2542441fcc
alxrgdriguez <sralexred@gmail.com>
2024-02-09 16:23:05 +0100

    Guardado

--- Tema5Prog/Practica_Objetos2/equipo_ciclistas/Ciclista.java
+++ Tema5Prog/Practica_Objetos2/equipo_ciclistas/Ciclista.java
@@ -1,7 +1,19 @@
 package Tema5Prog.Practica_Objetos2.equipo_ciclistas;
 
-public class Ciclista {
+abstract class Ciclista {
 
+    //Propiedades
+    protected int identificador;
 
+    protected String nombre;
 
+    protected double tiempoAcumulado;
+
+    //Constructor
+
+    public Ciclista(int identificador, String nombre) {
+        this.identificador = identificador;
+        this.nombre = nombre;
+        this.tiempoAcumulado = 0;
+    }
 }
