772832ccd6edaa7a0e3d22689e9b63e3acc9a88c
alxrgdriguez <sralexred@gmail.com>
2024-02-29 20:41:47 +0100

    Guardado

--- Practica_2TR_Ejercicios/Practicar_Ahorcado/TestAhorcado.java
+++ Practica_2TR_Ejercicios/Practicar_Ahorcado/TestAhorcado.java
@@ -1,4 +1,20 @@
 package Practica_2TR_Ejercicios.Practicar_Ahorcado;
 
+import java.util.Scanner;
+
 public class TestAhorcado {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Ahorcado juego = new Ahorcado();
+        int opcion = 0;
+
+        //Mostramos cuantas palabras tiene nuestro ahorcado
+        System.out.println("----- LETRAS QUE TIENE LA PALABRA -----");
+        System.out.println("La palabra elegida tiene " + juego.getPalabraAdivinar().length() + "letras");
+
+
+
+    }
 }
